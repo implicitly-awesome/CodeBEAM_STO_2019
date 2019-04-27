c3275948ef30b464bb451a1573b856fa9b7f7e58
Andrey Chernykh <andrei.chernykh@gmail.com>
2019-04-27 11:41:49 +0300

    exop deps

--- mix.exs
+++ mix.exs
@@ -42,7 +42,9 @@ defmodule SuperApp.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:exop, "~> 1.2.5"},
+      {:exop_data, "~> 0.1.6"}
     ]
   end
 
